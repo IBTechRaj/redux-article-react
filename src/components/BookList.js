1e402b5bf96180333cd29248898ba4c44d7912ef
ibtechraj <raj_shk@rediffmail.com>
2020-06-25 23:23:21 +0530

    used div to display data

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -2,23 +2,12 @@ import React from "react";
 
 // export default class BookList extends React.Component {
 const BookList = props => {
-  // constructor() {
-  //   super();
-  //   // this.state = {
-  //   //   books: this.props.books
-  //   // };
-  // }
-
-  // render() {
-  // let looks = [];
-  // looks = this.props.books;
-  // console.log("3", looks[0]);
   return (
     <ul>
-      <h2>{props.books.title}</h2>
-      {props.books.map(book => ({ book }))}
+      {props.books.map(book => (
+        <div>{book}</div>
+      ))}
     </ul>
   );
-  // }
 };
 export default BookList;
